efafd6af325a3da07f205a8aa4c682d490453d04
Sergio A <44273307+Sargaleano@users.noreply.github.com>
2019-04-25 18:09:22 -0500

    Corrections to experiment script

--- exps_bin_full.py
+++ exps_bin_full.py
@@ -3,11 +3,10 @@ from pupi_class import PupiBinary
 from pupi_bm import *
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 import csv
 
 ## General settings ##
-np.random.seed(int(str(int(time.time() * 1000))[-8:-1]))  # (91680801)  # Set random state for reproducibility
+np.random.seed(int(time.time()))  # (91680801)  # Set random state for reproducibility
 problems = [oneMax, squareWave, binVal]
 dims = [16, 25, 36, 64, 100]
 
@@ -16,53 +15,53 @@ nrates = [.25, .50, .75]
 evals = [10000, 20000, 40000]
 alphas = [.01, .1, .5]
 sigmas = [.01, .1, .5]
-stats = []; nreps = 50
+stats = []; nreps = 10
 
 for problem in problems:
     for dim in dims:
         for nrep in range(nreps):
             pupi = PupiBinary(fcost=problem, d=dim, viz=False)
-            results = pupi.optimise()
-            stats.append(results)
+            pupi.optimise()
+            stats.append(pupi.getResults())
             pupi.summary()
 print(stats)
 
-with open('csv/exps_dims.csv', 'a') as writeFile:
+with open('results/exps_dims.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerows(stats)
 writeFile.close()
-
-for problem in problems:
-    for n in popsize:
-        for nrep in range(nreps):
-            pupi = PupiBinary(fcost=problem, d=64, n=n, viz=False)
-            results = pupi.optimise()
-            stats.append(results)
-            pupi.summary()
-print(stats)
-# 
-with open('csv/exps_n.csv', 'a') as writeFile:
-  writer = csv.writer(writeFile)
-  writer.writerows(stats)
-writeFile.close()
-## save_csv(stats, "exps_dim.csv")
-#cwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-
-
-## save_csv(stats, "exps_dim.csv")
-
-for problem in problems:
-    for n in popsize:
-        for nrep in range(nreps):
-            pupi = PupiBinary(fcost=problem, d=100, n=n, viz=False)
-            results = pupi.optimise()
-            stats.append(results)
-            pupi.summary()
-print(stats)
-
-with open('csv/exps_n100.csv', 'a') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(stats)
-writeFile.close()
-## save_csv(stats, "exps_dim.csv")
+#
+# for problem in problems:
+#     for n in popsize:
+#         for nrep in range(nreps):
+#             pupi = PupiBinary(fcost=problem, d=64, n=n, viz=False)
+#             results = pupi.optimise()
+#             stats.append(results)
+#             pupi.summary()
+# print(stats)
+# #
+# with open('csv/exps_n.csv', 'a') as writeFile:
+#   writer = csv.writer(writeFile)
+#   writer.writerows(stats)
+# writeFile.close()
+# ## save_csv(stats, "exps_dim.csv")
+# #cwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#
+#
+#
+# ## save_csv(stats, "exps_dim.csv")
+#
+# for problem in problems:
+#     for n in popsize:
+#         for nrep in range(nreps):
+#             pupi = PupiBinary(fcost=problem, d=100, n=n, viz=False)
+#             results = pupi.optimise()
+#             stats.append(results)
+#             pupi.summary()
+# print(stats)
+#
+# with open('csv/exps_n100.csv', 'a') as writeFile:
+#     writer = csv.writer(writeFile)
+#     writer.writerows(stats)
+# writeFile.close()
+# ## save_csv(stats, "exps_dim.csv")
